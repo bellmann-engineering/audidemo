76e5876b014c99b01d27aab34a62aee3559fd4c8
Kai Bellmann <kb@bellmann-engineering.com>
2024-09-11 10:35:08 +0200

    added test

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    if (sum(2, 3) == 5) {
+        std::cout << "Test passed!" << std::endl;
+        return 0;
+    } else {
+        std::cout << "Test failed!" << std::endl;
+        return 1;
+    }
+}
